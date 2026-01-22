2aae32d9c4787af290268e02b09bf6ea84e10d41
Amin Gilani <amin@gilani.me>
2026-01-21 19:10:05 -0500

    Enable remnux ssh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
 sleep 2
 tailscale up ${TS_EXTRA_ARGS}
-exec tail -f /dev/null
+exec /usr/sbin/sshd -D
